0ddf7fc32942872b4efb5150f5d1b623ff7be2c1
hustlehard_yaral <70375267+facileomnibus@users.noreply.github.com>
2026-03-25 21:28:53 +0100

    Update widgets.js

--- widgets.js
+++ widgets.js
@@ -79,14 +79,18 @@ bar.appendChild(search)
    MENÚ RESPONSIVE
 ================================ */
 
-const menuBtn = document.querySelector(".menu-icon");
-const menu = document.querySelector(".navigation ul");
+const menuBtn=document.querySelector(".menu-icon")
+const menu=document.querySelector(".navigation ul")
 
-if (menuBtn && menu) {
-    menuBtn.addEventListener("click", function(e) {
-        menu.classList.toggle("show");
-    });
-}
+if(menuBtn && menu){
+
+menuBtn.addEventListener("click",()=>{
+menu.classList.toggle("show")
+})
+
+menuBtn.addEventListener("touchstart",()=>{
+menu.classList.toggle("show")
+})
 
 /* ===== FIX CLICK PC (evitar doble evento) ===== */
 
